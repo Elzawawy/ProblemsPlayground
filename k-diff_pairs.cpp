ce7d5acfa85ee6be2afee917a17acda09a7471c6
Elzawawy <amr22.elzawawy@gmail.com>
2019-04-09 09:00:50 +0200

    Added Problem 2: Mininmum Add Parentheses

--- k-diff_pairs.cpp
+++ k-diff_pairs.cpp
@@ -8,11 +8,7 @@
  * Here a k-diff pair is defined as an integer pair (i, j),
  * where i and j are both numbers in the array and their absolute difference is k.
  *
- * Example 1:
- * Input: [3, 1, 4, 1, 5], k = 2 , Output: 2
- * Explanation: There are two 2-diff pairs in the array, (1, 3) and (3, 5).
- * Although we have two 1s in the input, we should only return the number of unique pairs.
- *
+ * Find examples and more from here: https://leetcode.com/problems/k-diff-pairs-in-an-array/
  */
 
 /*****Attempt 1: Not fully working yet*****/
---
+++ minAdd_parentheses.cpp
@@ -0,0 +1,38 @@
+//
+// Created by zawawy on 4/9/19.
+//
+
+/* Problem statement:
+ * Given a string S of '(' and ')' parentheses,
+ * we add the minimum number of parentheses ( '(' or ')', and in any positions )
+ * so that the resulting parentheses string is valid.
+ * Formally, a parentheses string is valid if and only if:
+ * It is the empty string, or
+ * It can be written as AB (A concatenated with B), where A and B are valid strings, or
+ * It can be written as (A), where A is a valid string.
+ * Given a parentheses string, return the minimum number of parentheses
+ * we must add to make the resulting string valid.
+ *
+ * Examples and more can be found from: https://leetcode.com/problems/minimum-add-to-make-parentheses-valid/
+ *
+ */
+
+/*******Attempt 1: Accepted Solution in 8ms and 8.4 MB ********/
+
+int minAddToMakeValid(string S) {
+    int open =0;
+    int count=0;
+    for(char& c : S) {
+        if(c == '('){
+            open++;
+            count++;
+        }
+        else if(open > 0 && c==')'){
+            open--;
+            count--;
+        }
+        else
+            count++;
+    }
+    return count;
+}
