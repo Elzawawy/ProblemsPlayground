172e3c511ac4a6f420ac1df23bb6d85745702601
Elzawawy <amr22.elzawawy@ymail.com>
2020-04-05 01:05:28 +0200

    Solution of Parenting Partnering Returns improved.

--- src/codejam-2020/parenting_partnering_returns.cpp
+++ src/codejam-2020/parenting_partnering_returns.cpp
@@ -1,57 +1,63 @@
+////
+//// Created by zawawy on 4/4/20.
+////
+//#include <iostream>
+//#include <map>
+//#include <set>
+//#include <algorithm>
+//#include <vector>
+//#include <climits>
 //
-// Created by zawawy on 4/4/20.
+//#define start first.first
+//#define finish first.second
+//#define id second
+//using namespace std;
 //
-#include <iostream>
-#include <map>
-#include <set>
-#include <algorithm>
-#include <vector>
-
-using namespace std;
-
-int main()
-{
-    int T;
-    cin >> T;
-    int N[T];
-    for (int i = 0; i<T; i++) {
-        cin >> N[i];
-        vector<pair<pair<int, int>,int>> activities;
-        for (int j = 0; j<N[i]; j++) {
-            int temp_start, temp_end;
-            cin >> temp_start;
-            cin >> temp_end;
-            activities.emplace_back(make_pair(temp_start, temp_end),j);
-        }
-        sort(activities.begin(),activities.end());
-
-        int c_flag=-1, j_flag=-1;
-        bool is_impossible = false;
-        vector<char> result;
-        result.resize(static_cast<unsigned long>(N[i]));
-        for(int j=0;j<activities.size();j++){
-            // update flags each iteration on the activities.
-            if(activities[j].first.first >= c_flag)
-                c_flag = -1;
-            if(activities[j].first.first >= j_flag)
-                j_flag = -1;
-
-            if(c_flag == -1){
-                result[activities[j].second] = 'C';
-                c_flag = activities[j].first.second;
-            }
-            else if(j_flag == -1){
-                result[activities[j].second] = 'J';
-                j_flag = activities[j].first.second;
-            }
-            else{
-                is_impossible = true;
-                break;
-            }
-        }
-        if(is_impossible)
-            cout << "Case #" << i+1 << ": IMPOSSIBLE" << endl;
-        else
-            cout << "Case #" << i+1 << ": " << string(result.begin(), result.end()) << endl;
-    }
-}
+//int main()
+//{
+//    int T;
+//    cin >> T;
+//    int N[T];
+//    for (int i = 0; i<T; i++) {
+//        cin >> N[i];
+//        vector<pair<pair<int, int>, int>> activities;
+//        for (int j = 0; j<N[i]; j++) {
+//            int temp_start, temp_end;
+//            cin >> temp_start;
+//            cin >> temp_end;
+//            activities.emplace_back(make_pair(temp_start, temp_end), j);
+//        }
+//        sort(activities.begin(), activities.end());
+//
+//        int c_min_val = INT_MIN, j_min_val = INT_MIN;
+//        bool is_impossible = false;
+//        vector<char> result(N[i]);
+//        for (auto& activity : activities) {
+//            // update flags each iteration on the activities.
+//            if (activity.start>=j_min_val)
+//                j_min_val = INT_MIN;
+//            if (activity.start>=c_min_val)
+//                c_min_val = INT_MIN;
+//
+//            // assign activity to either Jaime or Cameron or No-one (Impossible Case)
+//            if (c_min_val==INT_MIN) {
+//                result[activity.id] = 'C';
+//                c_min_val = activity.finish;
+//            }
+//            else if (j_min_val==INT_MIN) {
+//                result[activity.id] = 'J';
+//                j_min_val = activity.finish;
+//            }
+//            else {
+//                is_impossible = true;
+//                // early break to improve performance.
+//                break;
+//            }
+//        }
+//        // print-out output according to activity status.
+//        if (is_impossible)
+//            cout << "Case #" << i+1 << ": IMPOSSIBLE" << endl;
+//        else
+//            cout << "Case #" << i+1 << ": " << string(result.begin(), result.end()) << endl;
+//    }
+//}
