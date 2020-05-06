9ba21190488504fa97500f5a43cef30f80d3eefd
Elzawawy <amr22.elzawawy@ymail.com>
2020-05-06 14:12:17 +0200

    Add Day 3 of May leetcoding Challenge: Ransom Note.

--- src/may-leetcoding-challenge/week-1/2_jewels_and_stores.cpp
+++ src/may-leetcoding-challenge/week-1/2_jewels_and_stores.cpp
@@ -1,7 +1,7 @@
 /**
  * Author: Amr Elzawawy
  * Date: 2-5-2020
- * Problem Name:  First Bad Version, Day 2 on the May LeetCoding Challenge.
+ * Problem Name:  Jewels and Stones, Day 2 on the May LeetCoding Challenge.
  */
 /* ### Problem:
  * Given: strings J representing the types of stones that are jewels, and S representing the stones you have. Each character in S is a type of stone you have.
---
+++ src/may-leetcoding-challenge/week-1/3_ransom_note.cpp
@@ -0,0 +1,38 @@
+/**
+ * Author: Amr Elzawawy
+ * Date: 3-5-2020
+ * Problem Name:  Ransom Note, Day 3 on the May LeetCoding Challenge.
+ */
+/* ### Problem:
+ * Given: Arbitrary ransom note string and another string containing letters from all the magazines.
+ * Required: Write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+ * Each letter in the magazine string can only be used once in your ransom note.
+ * Both strings contain only lowercase letters.
+ *
+ * Problem source and more about it can be found from: https://leetcode.com/problems/ransom-note/
+ */
+class Solution {
+public:
+    /** Solution 1: Using a HashMap to keep track of count of characters. [We can develop we similar approach with only a vector as well]
+     * Solution Runtime: 44 ms (Beats 57.94%)
+     * Solution Memory Usage: 8.8 MB (Beats 100%)
+     * @param ransomNote the string that has the ransom note
+     * @param magazine the string that has all the characters from magazines.
+     * @return whether we can re-construct ransom note from magazine or not.
+     */
+    bool canConstruct(string ransomNote, string magazine)
+    {
+        unordered_map<char, int> count_ransom(26);
+        // increase the count of characters in ransom note.
+        for (auto c: ransomNote)
+            count_ransom[c]++;
+        // for each character in the magazine string, if it is contained in the ransom decrease count.
+        for (auto c: magazine)
+            if (count_ransom.count(c)>0)
+                // the goal here is to erase the characters when it reaches zero count.
+                if (--count_ransom[c]==0)
+                    count_ransom.erase(c);
+        // if the ransom note can be constructed from magazine, then the counter map will be empty at the end.
+        return count_ransom.empty();
+    }
+};
